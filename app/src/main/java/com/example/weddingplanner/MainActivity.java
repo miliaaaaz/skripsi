03865df158b20cd815fff3b5ead6883b40f54021
BabyAyuAlicya <baby.alicya@binus.ac.id>
2023-10-29 16:31:22 +0700

    backend page signup

--- app/src/main/java/com/example/weddingplanner/MainActivity.java
+++ app/src/main/java/com/example/weddingplanner/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    // Create a new user with a first and last name
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     public void login(View view){
         startActivity(new Intent(getApplicationContext(),SignIn.class));
     }
+
+
 }
--- app/src/main/java/com/example/weddingplanner/SignIn.java
+++ app/src/main/java/com/example/weddingplanner/SignIn.java
@@ -1,16 +1,80 @@
 package com.example.weddingplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//INI PAGE SIGN UP SEBENERNYA
 
 public class SignIn extends AppCompatActivity {
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private EditText useremail, userpass;
+    private Button signup;
+    private ProgressDialog progressDialog;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        useremail = findViewById(R.id.useremail);
+        userpass = findViewById(R.id.userpass);
+//        signup = findViewById(R.id.signup);
+        progressDialog = new ProgressDialog(EditorActivity.this);
+        progressDialog.setTitle("Loading");
+        progressDialog.setMessage("Saving Data...");
+
+        signup.setOnClickListener(save->{
+            if (useremail.getText().length()>0 && userpass.getText().length()>0){
+                saveData(useremail.getText().toString(), userpass.getText().toString());
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Input Email and Password", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-}
+
+    private void saveData(String email, String password){
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", email);
+        user.put("password", password);
+
+        progressDialog.show();
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Saving Data Succeed", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                });
+        }
+
+    }
