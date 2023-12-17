affa166d92bdff4d1d360cfb8476b83709f9c0fa
miliaazmi <azmillia26@gmail.com>
2023-12-17 18:13:10 +0700

    baruuuuuuuuu

---
+++ app/src/main/java/com/example/weddingplanner/Todolist4.java
@@ -0,0 +1,14 @@
+package com.example.weddingplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Todolist4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todolist4);
+    }
+}
---
+++ app/src/main/java/com/example/weddingplanner/Todolist5.java
@@ -0,0 +1,14 @@
+package com.example.weddingplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Todolist5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todolist5);
+    }
+}
